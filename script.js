fd24d55903dc89daf2e6973fc2437361dedf74d7
Amanop2403 <amansachinshah@gmail.com>
2024-11-11 23:48:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('wishButton').addEventListener('click', function() {
     const messageDiv = document.getElementById('message');
     messageDiv.classList.toggle('hidden');
-    messageDiv.textContent = "Happy Birthday, Mom! 🎉🎂";
+    messageDiv.textContent = "To my first friend, my best friend and forever friend!! Happy Birthday, Mom! 🎉🎂";
 });
 
 document.getElementById('cakeButton').addEventListener('click', function() {
